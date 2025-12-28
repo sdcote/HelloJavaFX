fb5c74956d3506eb63c30898d91ab42447eca6b3
Steve Cote <sdcote@gmail.com>
2025-12-28 13:37:38 -0500

    Initial Load

---
+++ src/main/java/com/sdcote/Main.java
@@ -0,0 +1,7 @@
+package com.sdcote;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.utility {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports com.sdcote;
+}
